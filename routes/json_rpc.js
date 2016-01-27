e459e82eec6ba4ce51da77a06fd24f3bb5c46518
Jet <jetarin.min@gmail.com>
2016-01-27 20:03:21 +0700

    xxx

--- routes/json_rpc.js
+++ routes/json_rpc.js
@@ -10,10 +10,12 @@ router.post('/', function(req, res, next) {
     var body = req.body;
     if (body.method == 'execute') {
         console.log("RPC EXECUTE=============");
-        var model = body.params[0]
+        var model_name = body.params[0]
         var method = body.params[1]
         var args = body.params[2]
-        console.log("Model: "+model+"| Method: "+ method +"| Args: "+args);
+        console.log("Model: "+model_name+"| Method: "+ method +"| Args: "+args);
+
+        var model = require("../models/"+model_name);
     }
 });
 
