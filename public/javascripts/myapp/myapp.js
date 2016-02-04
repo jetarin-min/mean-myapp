cf7dbe0290606edc0fae2be5833a3f6affde5161
jetarin-min <jetarin.min@gmail.com>
2016-02-04 16:36:23 +0700

    update icom

--- public/javascripts/myapp/myapp.js
+++ public/javascripts/myapp/myapp.js
@@ -34,6 +34,17 @@ angular.module('myApp',
         }
     });
 }])
+.factory('socket', ['$rootScope', function($rootScope) {
+    var socket = io.connect('http://188.166.249.56:3009');
+        return {
+            on: function(eventName, callback){
+                socket.on(eventName, callback);
+            },
+            emit: function(eventName, data) {
+                socket.emit(eventName, data);
+            }
+        };
+}])
 .controller('MenuCtrl', function($scope, $rootScope){
     $scope.toggle_menu = function(){
         $rootScope.isToggleMenu = !$rootScope.isToggleMenu;
@@ -41,4 +52,20 @@ angular.module('myApp',
     $scope.toggle_chat = function(){
         $rootScope.isChat = !$rootScope.isChat;
     }
-});
+})
+.controller('ChatCtrl', function($scope, $rootScope, socket, $cookies){
+    $scope.close_chat = function(){
+        console.log("Close Chat");
+        $rootScope.isChat = false;
+    }
+    $scope.send_text = function() {
+        socket.emit('send-text', $cookies.get("username")+": "+$scope.chat.text);
+        $scope.chat.text = "";
+    };
+    socket.on('broadcast-text', function(data) {
+        console.log("Incomming message: "+data);
+        old_msg = document.getElementById("msg_box").innerHTML;
+        document.getElementById("msg_box").innerHTML = old_msg + "<p>"+data.message+"</p>";
+    });
+})
+console.log("CHAT");
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.statusCode = 302; 
+    res.setHeader("Location", "/login");
+    res.end();
 });
 
 module.exports = router;
